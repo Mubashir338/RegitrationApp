c7c5b7fe717c926f9717f66c4103184257edfa86
Mubashir338 <79444924+Mubashir338@users.noreply.github.com>
2021-11-06 17:21:19 +0500

    Link established

--- app/src/main/java/com/example/regitrationapp/MainActivity2.java
+++ app/src/main/java/com/example/regitrationapp/MainActivity2.java
@@ -2,18 +2,34 @@ package com.example.regitrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
 
-    public void link(View view) {
+        button = findViewById(R.id.button2);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://github.com/Mubashir338/RegitrationApp");
+            }
+
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW, uri));
+            }
+        });
     }
 }
